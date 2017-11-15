7fa62c19fc9eea27a259429e4156a4353ae9c328
fitsum belayneh <fitsu_alt@yahoo.com>
2017-11-14 22:01:21 -0500

    keys.js eddited

--- keys.js
+++ keys.js
@@ -1,10 +1,10 @@
 console.log('this is loaded');
 
 var twitterKeys = {
-  consumer_key: '<eS4feOcmeqsc4SevZbzHyv1WE>',
-  consumer_secret: '<	yNAqB1clUKd1q9n5xQ39hTQcc521tf2F9g9v9Qh0XCxT1NuETX>',
-  access_token_key: '<1735907990-3qSC9jOoLXXdXHUoafwlJ6yAUvHLJT8P1Pmn5YI>',
-  access_token_secret: '<	fhWrrJAMWzGMUipUk6sZlY1ncHpeJnHWNj3LwAtutUHsb>',
+  consumer_key: 'eS4feOcmeqsc4SevZbzHyv1WE',
+  consumer_secret: 'yNAqB1clUKd1q9n5xQ39hTQcc521tf2F9g9v9Qh0XCxT1NuETX',
+  access_token_key: '1735907990-3qSC9jOoLXXdXHUoafwlJ6yAUvHLJT8P1Pmn5YI',
+  access_token_secret: 'fhWrrJAMWzGMUipUk6sZlY1ncHpeJnHWNj3LwAtutUHsb',
 }
 
 module.exports = twitterKeys;
